563d713873a463332d07dc0cb902c597cab50834
wangxin <wangxin1@ihandy.cn>
2016-11-27 15:42:58 +0800

    no message

--- testProject/11.spring3/src/main/java/com/wangxin/app/TestThread1.java
+++ testProject/11.spring3/src/main/java/com/wangxin/app/TestThread1.java
@@ -1,20 +1,37 @@
-//package com.wangxin.app;
-//
-//import org.apache.commons.lang.time.StopWatch;
-//
-///**
-// * Created by Administra on 2016/11/8.
-// */
-//public class TestThread1 extends Thread {
-//
-//    public void  run(){
-//        String url = "http://localhost:8084/upload/test";
-//        StopWatch watch = new StopWatch();
-//        // 计时器开始
-//        watch.start();
-//        String str = HttpUtil.httpGet(url);
-//        watch.stop();
-//        System.out.println( watch.getTime()+Thread.currentThread().getName());
-//    }
-//
-//}
+package com.wangxin.app;
+
+import com.wangxin.app.util.http.HttpClientUtil;
+import com.wangxin.app.util.http.HttpClientUtils;
+import org.apache.commons.lang.time.StopWatch;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+
+import java.util.Random;
+
+/**
+ * Created by Administra on 2016/11/8.
+ */
+public class TestThread1 extends Thread {
+
+    public void  run(){
+        StopWatch watch = new StopWatch();
+        // 计时器开始
+        watch.start();
+        String str = HttpClientUtil.httpGet("http://172.16.60.91:8013/yhapi/restful/account/queryaccostate", null);
+        watch.stop();
+        System.out.println( watch.getTime()+Thread.currentThread().getName());
+    }
+
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 1000; i++) {
+            TestThread1 t1 = new TestThread1();
+            t1.start();
+        }
+//        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+
+}
